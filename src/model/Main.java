a20e517e57d95982d694cfc2631075bc63897c4d
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2024-04-16 14:22:31 -0500

    Modificaciones dentro del algoritomo de entrada y tratamiento de datos

--- src/model/Main.java
+++ src/model/Main.java
@@ -8,14 +8,14 @@ public class Main {
     public static void main(String[] args) {
         Logica1 logica1 = new Logica1();
         funcionVectorial funcionVecto=crearFuncionVectorial();
-        System.out.println(funcionVecto.getTendenciaLimite());
+        System.out.println("esta es la tendencia limite: "+funcionVecto.getTendenciaLimite());
         boolean comprobar=logica1.validarArregloFuncionVectorial(funcionVecto.getFunciones());
         if(comprobar){
             String valMostrar="";
             double[] limites = new double[3];
             for (int i = 0; i <funcionVecto.getFunciones().length; i++) {
                 double limite=logica1.calcularLimite(funcionVecto.getTendenciaLimite(),funcionVecto.getFunciones()[i]);
-                System.out.println(logica1.evaluarFuncion(funcionVecto.getTendenciaLimite(),funcionVecto.getFunciones()[i]));
+                System.out.println(" esta es la evaluacion de la funcion "+funcionVecto.getFunciones()[i]+" :"+logica1.evaluarFuncion(funcionVecto.getTendenciaLimite(),funcionVecto.getFunciones()[i]));
                 limites[i]= limite;
                 valMostrar=valMostrar+" "+limite+" ";
             }
@@ -26,7 +26,7 @@ public class Main {
         }else{
             System.out.println("no cumple con los requisitos");
         }
-
+        System.out.println(Arrays.toString(funcionVecto.getFunciones()));
         System.out.println(Arrays.toString(funcionVecto.getLimite()));
 
     }
